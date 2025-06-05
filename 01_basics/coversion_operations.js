e6859b21b01254c501a6151dc8472bdbcb219c82
Muhammad Sadiq <muhammadsadiqrajani@gmail.com>
2025-06-05 23:28:01 +0500

    Coversions

---
+++ 01_basics/coversion_operations.js
@@ -0,0 +1,20 @@
+let score = "55acv"
+
+console.log(score)
+console.log(typeof (score))
+
+let score_in_num = Number(score)
+
+console.log(score_in_num)
+console.log(typeof (score_in_num))
+
+
+let some_num = 33
+
+console.log(String(some_num))
+console.log(typeof String(some_num))
+
+
+
+console.log(Boolean(some_num))
+console.log(typeof Boolean(some_num))
